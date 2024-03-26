dba4c655b61b0a7d544fac55c92b5fa07d89ad81
kawinwatleo <6633010521@student.chula.ac.th>
2024-03-26 15:41:13 +0700

    Authentication

--- src/libs/userRegister.tsx
+++ src/libs/userRegister.tsx
@@ -1,5 +1,5 @@
 export default async function userRegister(userUsername:string,userName:string,userTel:string,userEmail:string,userPassword:string,userRole:string){
-    const response = await fetch("https://hotel-booking-backend-git-main-kawinwats-projects.vercel.app/api/v1/auth/register",{
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/auth/register`,{
         method:"POST",
         headers: {
             "Content-Type":"application/json"
