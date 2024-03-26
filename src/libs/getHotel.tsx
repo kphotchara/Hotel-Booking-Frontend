5f850adcb44b020a08c4e02f0f401d860ed949e0
leopwrg <147370712+leopwrg@users.noreply.github.com>
2024-03-26 18:09:39 +0700

    Authentication

--- src/libs/getHotel.tsx
+++ src/libs/getHotel.tsx
@@ -1,5 +1,5 @@
 export default async function getHotel(hid:string){
-    const response = await fetch(`https://hotel-booking-backend-git-main-kawinwats-projects.vercel.app/api/v1/hotels/${hid}`,{cache:'no-store'})
+    const response = await fetch(`${process.env.BACK_END_URL}/api/v1/hotels/${hid}`,{cache:'no-store'})
     if(!response.ok){
         throw new Error("failed to fetch hotel") 
     } 
