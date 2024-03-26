4a3c6108ff2fa02d4dbfdc000a0b9f4f1c558cfe
Photchara Kallayanasiri <photcharakallayanasiri@Photcharas-MacBook-Air.local>
2024-03-26 19:01:57 +0700

    fix border hotel img

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,14 +7,13 @@ import React from 'react';
 
 export default function Card({hotelName , imgSrc ,district,province, rating,price}:{hotelName:string , imgSrc:string ,district:string,province:string, rating:number,price:number}){
     
-
     return (
         <InteractiveCard contentName={hotelName}>
             <div className='w-1/2 h-full relative rounded-t-lg'>
                 <Image src={imgSrc?imgSrc:'/img/no-image.jpg'} 
                 alt='Product Picture'
                 fill={true}
-                className='object-cover rounded-t-lg'/>
+                className='object-cover rounded-l-lg'/>
             </div>
             <div className='w-1/2 h-full py-6 px-5 font-Poppins flex flex-col space-y-4 text-black'>
                 <div className='text-2xl font-semibold '>{hotelName}</div>
