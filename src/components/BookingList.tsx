4b864afdd82dac0ab99e7d376fb921bb92873a4e
Your Name <you@example.com>
2024-03-27 20:20:30 +0700

    add DELETE booking api(not successful)

--- src/components/BookingList.tsx
+++ src/components/BookingList.tsx
@@ -4,10 +4,37 @@ import { removeBooking } from "@/redux/features/bookSlice"
 import { useAppSelector , AppDispatch} from "@/redux/store"
 import { useDispatch } from "react-redux"
 import { useRouter } from "next/navigation"
+import addOneBooking from "@/libs/addOneBooking";
+import deleteBooking from "@/libs/deleteBooking"
+import DateReserve from "@/components/DateReserve";
+import { useSession } from "next-auth/react";
+import dayjs, {Dayjs} from "dayjs";
+import { useState } from "react"
+import { useSearchParams } from "next/navigation"
+
+
 export default function BookingList () {
+    const urlParams = useSearchParams()
     const bookingItems = useAppSelector ((state)=> state.bookSlice.bookItems)
     const dispatch = useDispatch<AppDispatch>()
     const router = useRouter()
+    const hName = urlParams.get('hotel')
+    const hid = urlParams.get('id') || '';
+    const {data:session} =useSession();
+   
+    const handleRemoveBooking = async (bookingId: string) => {
+        try {
+            if (session && session.user && session.user.token) {
+                await deleteBooking(session.user.token, bookingId); // Call removeBooking with the bookingId
+                alert("Delete your booking successfully !! ");
+            } else {
+                alert("Failed to delete your booking");
+            }
+        } catch (error) {
+            console.error("Error deleting booking:", error);
+            alert("Failed to delete your booking");
+        }
+    };
     return(
         <>
         {
@@ -26,7 +53,8 @@ export default function BookingList () {
                     edit
                 </button>
                 <button className="block rounded-2xl bg-black hover:bg-indigo-600 px-5 py-2
-                text-white text-sm shadow-sm absolute right-4 bottom-4" onClick = {() => dispatch(removeBooking(bookItem._id))}>
+                text-white text-sm shadow-sm absolute right-4 bottom-4" onClick = {() =>{dispatch(removeBooking(bookItem._id)); 
+                    handleRemoveBooking(bookItem.bookingID)} }>
                     Remove
                 </button>
                 </div>
